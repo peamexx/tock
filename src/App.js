e4bd517feb0e62d3407254fdea58351a2ba00479
Roy King <peamexx@gmail.com>
2021-01-30 19:15:41 +0900

    Add StockIndex.js, MostPopularNews.js /  Modify Home.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './common.css';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
-// import { Bar } from 'react-chartjs-2';
 
 function App() {
   return (
---
+++ src/MostPopularNews.js
@@ -0,0 +1,25 @@
+function MostPopularNews() {
+    return (
+        <div>
+            <h3>가장 많이 본 뉴스</h3>
+            <span>
+                <a href="#" className="thumb">
+                    <img src="https://t1.daumcdn.net/news/202101/30/moneytoday/20210130150903307vkkg.jpg" alt="이미지1"/>
+                </a>
+                <ul>
+                    <li><a href="#" className="tit">[마감시황]코스피 3% 급락하며 3,000선 무너져</a></li>
+                    <li><a href="#" className="tit">고개 숙인 공매도의 전설..시트론 대표 "다시는 공매도 리포트 안낸다" [株포트라이트]</a></li>
+                    <li><a href="#" className="tit">[특징주] 노바백스 관련주 주목.. 'SK케미칼·디알젬·켐온'</a></li>
+                    <li><a href="#" className="tit">게임스탑 공매도 사태에 웃었다..어부지리 급등 종목 속출 [株포트라이트]</a></li>
+                    <li><a href="#" className="tit">[시나쿨파] 조직화된 개미들, 자본주의 심장을 뒤흔들다</a></li>
+                </ul>
+                <a href="#" className="ads">
+                    <img src="/adsKakaoCareer.jpg" alt="광고 이미지"/>
+                    <p>2021년 공채 정보 확인하자!</p>
+                </a>
+            </span>
+        </div>
+    );
+};
+
+export default MostPopularNews;
