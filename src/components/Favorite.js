6912477d41c0be4cf98ff9353adc50af3643f3ad
Roy King <peamexx@gmail.com>
2021-02-06 19:59:28 +0900

    Modify 정렬 기능 추가

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,4 +1,5 @@
 import { Add, ImportExport, Create } from '@material-ui/icons';
+import { useState } from 'react';
 import Item from './Item';
 
 function Favorite() {
@@ -30,7 +31,7 @@ function Favorite() {
       sub: 0,
       price: 347500,
       volumn: 1886521,
-      chgp: 8000,
+      chgp: -8000,
       chgr: -2.25,
       upDown: 'nag',
     },
@@ -40,7 +41,7 @@ function Favorite() {
       sub: -1,
       price: 30500,
       volumn: 1597540,
-      chgp: 900,
+      chgp: -900,
       chgr: -2.87,
       upDown: 'nag',
     },
@@ -56,19 +57,58 @@ function Favorite() {
     },
   ];
 
+  let [stock, setStock] = useState(stockItem);
+
+  const sortListOn = () => {document.querySelector('.layer').classList.add('on')};
+
+  // 현재가
+  const upPrice = () => {
+    let sorted = [...stock].sort((a, b) => b.price - a.price);
+    setStock(sorted);
+    document.querySelector('.layer').classList.remove('on');
+  };
+
+  // 거래량
+  const upVolumn = () => {
+    let sorted = [...stock].sort((a, b) => b.volumn - a.volumn);
+    setStock(sorted);
+    document.querySelector('.layer').classList.remove('on');
+  };
+
+  // 등락률
+  const upChgr = () => {
+    let sorted = [...stock].sort((a, b) => b.chgr - a.chgr);
+    setStock(sorted);
+    document.querySelector('.layer').classList.remove('on');
+  };
+
+  // 등락
+  const upChgp = () => {
+    let sorted = [...stock].sort((a, b) => b.chgp - a.chgp);
+    setStock(sorted);
+    document.querySelector('.layer').classList.remove('on');
+  };
+
+  // 이름
+  const upName = () => {
+    let sorted = [...stock].sort((a, b) => a.name > b.name ? 1 : -1);
+    setStock(sorted);
+    document.querySelector('.layer').classList.remove('on');
+  };
+
   return (
     <div className="favorite">
       <div className="inner">
         <div className="setting">
           <ul>
             <li><Add fontSize="large"/><span>종목추가</span></li>
-            <li><ImportExport fontSize="large"/><span>정렬</span></li>
+            <li onClick={ sortListOn }><ImportExport fontSize="large"/><span>정렬</span></li>
             <li><Create fontSize="large"/><span>변경</span></li>
           </ul>
         </div>
         <div className="list">
           <table>
-            { stockItem.map((item) => 
+            { stock.map((item) => 
               <Item 
               name={ item.name } 
               code={ item.code } 
@@ -82,9 +122,24 @@ function Favorite() {
             ) }
           </table>
         </div>
+        <Modal />
       </div>
     </div>
   );
+
+  function Modal() {
+    return (
+      <div className="layer">
+        <ul className="sortList">
+          <li onClick={ upPrice }>현재가</li>
+          <li onClick={ upVolumn }>거래량</li>
+          <li onClick={ upChgr }>등락률</li>
+          <li onClick={ upChgp }>등락</li>
+          <li onClick={ upName }>이름</li>
+        </ul>
+      </div>
+    );
+  };
 }
 
 export default Favorite;
