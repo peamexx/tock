9ca7701e53f00281687dc438f34780286b55de7f
peamexx <peamexx@gmail.com>
2021-02-16 19:31:36 +0900

    Modyfiy link 수정

--- src/components/OpinionItem.js
+++ src/components/OpinionItem.js
@@ -1,16 +1,17 @@
 import FavoriteTwoToneIcon from '@material-ui/icons/FavoriteTwoTone';
 import SentimentDissatisfiedTwoToneIcon from '@material-ui/icons/SentimentDissatisfiedTwoTone';
+import { Link } from 'react-router-dom';
 
 function OpinionItem(props) {
     return (
         <tr key={ props.id }>
             <td className="subject">{ props.subject }</td>
             <td className="tit">
-                <a href="/opinionPost">
+                <Link to="/opinionPost">
                     <strong className="stockName">[{ props.stockName }]</strong>
                     <span className="title">{ props.title }</span>
                     <span className="replyCount">[{ props.replyCount }]</span>
-                </a>
+                </Link>
             </td>
             <td className="name">{ props.name }</td>
             <td className="date">{ props.date }</td>
