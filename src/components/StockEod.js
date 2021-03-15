74656dbc494f2f4a98c7f3e48d2f5fb57ff33a65
peamexx <peamexx@gmail.com>
2021-03-15 17:04:04 +0900

    [UPDATE] api받는 사이트 변경(https 적용), css 일부 수정

--- src/components/StockEod.js
+++ src/components/StockEod.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from 'react'
 import StockEodItem from './StockEodItem'
 
 function StockEod() {
-    const apiUrl = 'http://api.marketstack.com/v1/eod';
-    let apiSymbol = 'AAPL';
-    let queryParams = '?access_key=' + '329f9ecb5ff8b44c0fafdea135a1beb0';
-    queryParams += '&symbols=' + apiSymbol;
+    const options = {
+        method: 'GET',
+        url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
+        params: {symbol: 'AAPL', region: 'US'},
+        headers: {
+          'x-rapidapi-key': 'b1187ee220msh6d87d66445d6aa9p18f94bjsn8d17e3bafaa6',
+          'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+        }
+      };
 
     let content;
-    let ydaySymbol;
     let ydayClose;
-    let exchange;
     let ydayVolume;
     let ydayOpen;
     let ydayHigh;
@@ -21,76 +24,54 @@ function StockEod() {
 
     // api 받기
     useEffect(() => {
-        axios.get(apiUrl + queryParams)
-            .then((response) => {
-                setEodData(response.data);
-            })
-    }, [apiUrl]);
-
-    // <!--api 형태-->
-    // adj_close: 121.085 
-    // adj_high: 122.06 
-    // adj_low: 118.79 
-    // adj_open: 119.03 
-    // adj_volume: 129525780 
-    // close: 121.085 
-    // date: "2021-03-09T00:00:00+0000"
-    // exchange: "XNAS" 
-    // high: 122.06
-    // low: 118.79
-    // open: 119.03
-    // symbol: "AAPL"
-    // volume: 129525080
+        axios.request(options).then((response) => {
+            setEodData(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }, [options.url]);
 
     if(eodData) { // api 데이터 다 불러오면 그 다음에 작업
-        ydaySymbol = eodData.data[0].symbol; // 심볼
-        ydayClose = eodData.data[0].adj_close.toFixed(2); // 최근 전일 종가
-        exchange = eodData.data[0].exchange; // 거래소
-        ydayVolume = eodData.data[0].adj_volume.toLocaleString(); // 최근 거래량
-        ydayHigh = eodData.data[0].adj_high.toFixed(2); // 최근 고가
-        ydayOpen = eodData.data[0].adj_open.toFixed(2); // 최근 전일 시가
-        ydayLow = eodData.data[0].adj_low.toFixed(2); // 최근 저가
+        ydayClose = eodData.prices[0].adjclose.toFixed(2); // 최근 전일 종가
+        ydayVolume = eodData.prices[0].volume.toLocaleString(); // 최근 거래량
+        ydayHigh = eodData.prices[0].high.toFixed(2); // 최근 고가
+        ydayOpen = eodData.prices[0].open.toFixed(2); // 최근 전일 시가
+        ydayLow = eodData.prices[0].low.toFixed(2); // 최근 저가
 
-        if(exchange === 'XNAS') {
-            exchange = 'NASDAQ';
-        } else if(exchange === 'XNYS') {
-            exchange = 'NYSE'
-        };
-
-        content = eodData.data.map((item, index, arr) => {
+        content = eodData.prices.map((item, index, arr) => {
             if(index > arr.length - 2) return;
 
             // 전날 데이터(전일대비, 등락율 구하기 위해 필요)
             let prevData = arr[index + 1];
 
-            // 날짜 변경 2021-03-09T00:00:00+0000 ---> 03/09
-            let date = item.date;
-            let month = date.slice(5, 7); // 03
-            let day = date.slice(8, 10); // 09
-            date = `${month}/${day}`;
-
             return <StockEodItem 
                     key={ item.volume }
                     id={ item.volume }
-                    date={ date }
-                    close={ item.adj_close.toFixed(2) }
-                    chgp={ (item.adj_close - prevData.adj_close).toFixed(2) } // 오늘 종가 - 전일 종가
-                    chgr={ ((((item.adj_close - prevData.adj_close) / item.close).toFixed(4)) * 100).toFixed(2) } // 전일대비 - 전일 종가
-                    volume={ item.adj_volume.toLocaleString() }
+                    date={ unixTimeConvert(item.date) }
+                    close={ Number(item.adjclose).toFixed(2) }
+                    chgp={ (item.adjclose - prevData.adjclose).toFixed(2) } // 전일대비 = 오늘 종가 - 전일 종가
+                    chgr={ (((item.adjclose - prevData.adjclose) / item.adjclose).toFixed(4) * 100).toFixed(2) } // 등락율 = 전일대비 - 전일 종가
+                    volume={ Number(item.volume).toLocaleString() }
                     />;
             });
-        
-        // let chgp = document.querySelectorAll('.chgp');
-        // if(chgp[0]) {
-        //     chgp.map((item) => {
-        //         if(Number(item.textContent) < 0) {
-        //             // item.classList.add('eieiei');
-        //             console.log('음수')
-        //         } else {
-        //             console.log('양수?')
-        //         }
-        //     });
-        // }
+
+        // 전일대비, 등락율이 음수/양수 ---> .nag/.pos 추가
+        let chgpAll = document.querySelectorAll('.chgp');
+        if(chgpAll) {
+            console.log(chgpAll.length);
+            chgpAll.forEach((item) => {
+                if(Number(item.textContent) < 0) {
+                    console.log(Number(item.textContent));
+                    item.classList.add('nag');
+                    item.parentElement.querySelector('.chgr').classList.add('nag'); // chgr
+                } else if(Number(item.textContent) > 0) {
+                    item.classList.add('pos');
+                    item.parentElement.querySelector('.chgr').classList.add('pos'); // chgr
+                };
+            });
+        } else {
+            console.log('없음ㄴㄴㄴ')
+        }
     } else {
         <span>데이터 로딩중입니다</span>
     };
@@ -101,11 +82,14 @@ function StockEod() {
                 <div className="top">
                     <p>해당 페이지는 "애플" 종목 일별 페이지로 일괄 처리됩니다</p>
                 </div>
+                <div className="top top2">
+                    <p>api 내려받는데 5초가량 시간이 걸립니다</p>
+                </div>
                 <div className="title">
                     <div className="left">
                         <span className="tit">애플</span>
-                        <span className="sym">{ ydaySymbol }</span>
-                        <span className="exchange">{ exchange }</span>
+                        <span className="sym">AAPL</span>
+                        <span className="exchange">NASDAQ</span>
                     </div>
                     <div className="right">
                         <span className="open">{ ydayClose }</span>
@@ -133,20 +117,32 @@ function StockEod() {
                 </div>
                 <div className="eodData">
                     <table>
-                        <table></table>
-                        <tr>
-                            <th>일자</th>
-                            <th>종가</th>
-                            <th>전일대비</th>
-                            <th>등락율</th>
-                            <th>거래량</th>
-                        </tr>
-                        { content }
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <th>일자</th>
+                                <th>종가</th>
+                                <th>전일대비</th>
+                                <th>등락율</th>
+                                <th>거래량</th>
+                            </tr>
+                            { content }
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
     );
+};
+
+function unixTimeConvert(dataPara) {
+    let date = new Date(dataPara * 1000);
+    let year = date.getFullYear();
+    let month = (date.getMonth() + 1);
+    let day = date.getDate();
+
+    let timeConvered = `${year}/${month}/${day}`;
+    return timeConvered;
 };
 
 export default StockEod;
